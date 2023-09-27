af28bbdf2a1ba97a61424a9c62918b1b14653f4c
Sveinung Hjelmervik <82393672+Svehini@users.noreply.github.com>
2023-09-27 09:40:55 +0200

    Update LongestSubstringWithoutRepeatingCharacters.py

--- LongestSubstringWithoutRepeatingCharacters.py
+++ LongestSubstringWithoutRepeatingCharacters.py
@@ -17,3 +17,36 @@ class Solution:
         for i in range(len(s)):
             lengthForEachChar.append(self.getLength(s[i:]))
         return max(lengthForEachChar)
+
+
+# 2nd solution which is a little faster
+
+
+class Solution:
+    def getLength(self, first, listStr):
+        listStr = listStr.split(first)
+        for i in range(len(listStr)):
+            string = list(dict.fromkeys(listStr[i]))
+            if len(string) != len(listStr[i]):
+                used_chars = []
+                word = listStr[i]
+                for n in range(len(word)):
+                    if word[n] not in used_chars:
+                        used_chars.append(word[n])
+                    else:
+                        listStr[i] = word[:n]
+                        break
+        for n in range(len(listStr)):
+            listStr[n] = len(listStr[n])
+        return (max(listStr)+1)
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if s == "":
+            return 0
+        lengthForEachChar = []
+        used_char = []
+        for i in range(len(s)):
+            if s[i] not in used_char:
+                used_char.append(s[i])
+                lengthForEachChar.append(self.getLength(s[i], s[i:]))
+        return max(lengthForEachChar)
